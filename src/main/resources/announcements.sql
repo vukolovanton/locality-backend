299e4ad77979a4012f960ab13bfe50942c3384f4
Anton Vukolov <vukolovanton92@gmail.com>
2021-08-26 19:26:00 +0400

    Use Postgress

--- src/main/resources/announcements.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `announcements` (`id`, `created_at`, `updated_at`, `description`, `locality_id`, `status`, `title`, `type`, `user_id`, `is_pinned`, `image_url`)
-VALUES
-(88, '2021-08-20 11:53:00.976813', '2021-08-20 11:53:00.976952', 'TestTestTest', 1, 'ACTIVE', 'Test 1', NULL, 2, 0, 'http://rum6s.jpg');
