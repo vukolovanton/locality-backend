92bbd15613a5f960b76d2325bf65c496d4fd743b
Anton Vukolov <vukolovanton92@gmail.com>
2021-08-26 22:49:38 +0400

    Switch to java 11

--- src/main/java/com/backend/locality/api/users/UserDetailsImpl.java
+++ src/main/java/com/backend/locality/api/users/UserDetailsImpl.java
@@ -6,13 +6,11 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.Serial;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
-    @Serial
     private static final long serialVersionUID = 1L;
 
     private final Long id;
